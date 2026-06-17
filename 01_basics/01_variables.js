d00a44dbdae0b6447a905babd5fd600b6ceed53f
shikshameena070-glitch <shikshameena070@gmail.com>
2026-06-17 16:10:02 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "shikshameena.com"
+var accountPassword = "12345"
+accountCity = "Vidisha"
+
+
+//accountId = 2 //not allowed
+
+accountEmail = "fhh@3.com"
+accountPassword = "12121"
+accountCity = "bhopal"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
