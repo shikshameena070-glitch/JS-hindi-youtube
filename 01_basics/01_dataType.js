357623f3801ae387236794782f654e3ef3762d61
shikshameena070-glitch <shikshameena070@gmail.com>
2026-06-18 15:31:46 +0000

    datatypes

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") //we are using node.Js noy browser
+
+console.log(3+3); console.log("shiksha")
+
+//primitive data type
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//NULL => standalone value
+//undefined =>
+//symbol => unique
+//object
+
+console.log(typeof "shiksha");
