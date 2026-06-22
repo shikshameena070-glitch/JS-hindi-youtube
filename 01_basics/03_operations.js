4b0662e15aa6eb3da3582929be8cd25822cc6f88
shikshameena070-glitch <shikshameena070@gmail.com>
2026-06-22 15:55:53 +0000

    operations of basics

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(5-2);
+console.log(2*2);
+console.log(10/2);
+console.log(11%2);
+console.log(2**3);*/
+
+let str1 = "Shiksha"
+let str2 = " Meena"
+
+console.log(str1+str2);
+
+/*
+console.log("1" + 2);
+console.log( 1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");*/
+
+
+console.log(+true);
+console.log(+"");
