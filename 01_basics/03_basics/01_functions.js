31e86bc70c915fa8567cfaa1760157787f806f8d
shikshameena070-glitch <shikshameena070@gmail.com>
2026-06-28 07:23:48 +0000

    functions

---
+++ 01_basics/03_basics/01_functions.js
@@ -0,0 +1,20 @@
+
+
+/*function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("K");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+}*/
+
+sayMyName//refference
+//sayMyName()//execution
+
+function addTwoNumbers(number1,number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3,4)
