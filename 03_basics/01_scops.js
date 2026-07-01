255e2426b851fc4bc6315e00924afd307cb420b8
shikshameena070-glitch <shikshameena070@gmail.com>
2026-07-01 16:19:51 +0000

    scops

---
+++ 03_basics/01_scops.js
@@ -0,0 +1,53 @@
+//var c=300
+/*let a = 300
+
+if(true){
+let a = 10
+const b = 20
+//var c = 30
+console.log("INNER:",a);
+}
+console.log(a);
+//console.log(b);
+//console.log(c);
+
+for(let i=0;i<Array.length;i++){
+    const element = array[i];
+}*/
+
+/*function one(){
+    const username = "shiksha"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+one()*/
+
+if(true){
+    const username = "shiksha"
+    if(username === "shiksha"){
+        const website = "Meena"
+       // console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username)
+
+//++++++++++++++++++++++++++++++ intresting ++++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+
+//console.log(addTwo(5));
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5));
