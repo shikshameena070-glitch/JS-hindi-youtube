fde0a620f7269fb02bcad4ab3e660ef9c4ee1ed5
shikshameena070-glitch <shikshameena070@gmail.com>
2026-07-07 16:07:17 +0000

    truely

---
+++ 04_controlFlow/truethy.js
@@ -0,0 +1,40 @@
+/*const userEmail = "shiksha@23.com"
+
+if(userEmail){
+    console.log("Got user email");
+    
+}else{
+    console.log("Don't have user email");
+}*/
+
+// falsy value => false, 0, -0 , BigInt 0n , "" , null , undefined , NaN
+// truethy values => "0" , 'false' , " " , [] , {} , function(){}
+
+/*if(userEmail.length === 0){
+    console.log("Array is empty");
+    
+}*/
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 10
+
+console.log(val1)
+
+
+// Terniary Operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("mare than 80")
